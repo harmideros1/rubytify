720eac3dfb9c198b5792ab5c12dbd8e8117a4c7f
Harold <harold.mideros@gmail.com>
2021-05-23 18:58:20 -0500

    Refactor artists controller

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,20 +1,16 @@
-class ArtistsController < ApplicationController
-  before_action :set_artist, only: [:show, :albums]
+class Api::V1::ArtistsController < ApplicationController
+  before_action :set_artist, only: [:albums]
 
-  # GET /artists
+  # GET /api/v1//artists
   def index
     @artists = Artist.all.order(popularity: :desc)
-    render json: @artists
+    # render json: @artists
+    render json: { data: @artists.as_json(only: %w[id name image genres popularity spotify_url]) }
   end
 
-  # GET /artists/1
-  def show
-    render json: @artist
-  end
-
-  # GET /artists/1/albums
+  # GET /api/v1//artists/1/albums
   def albums
-    render json: @artist.albums.as_json
+    render json: { data: @artist.albums.as_json(only: %w[id name image spotify_url total_tracks]) }
   end
 
 
