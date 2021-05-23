4fed43b997a0b6293cf99fbddb8eb8a3fe89ed77
Harold <harold.mideros@gmail.com>
2021-05-23 18:59:44 -0500

    Make routing for requirements

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :artists, only: [:index, :albums]
+      get 'artists/:id/albums', to: 'artists#albums'
+
+      resources :albums, only: [:index, :show]
+      get 'albums/:id/songs', to: 'albums#show'
+
+
+      resources :songs, only: [:index, :random]
+      get 'genres/:genre_name/random_song', to: 'songs#random'
+    end
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
